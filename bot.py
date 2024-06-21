fedea36a8db87541447ef6a9a071d1480a801061
Vonard <129731100+Vonard@users.noreply.github.com>
2024-06-21 16:21:47 +0300

    Photos are published automatically

--- bot.py
+++ bot.py
@@ -1,5 +1,25 @@
 import telegram
+import os
+import random
+import time
+import argparse
 bot = telegram.Bot(token="7245572263:AAHRGAQE3v8EN4wUvTiKiLONyTiYiCBHLJI")
-print(bot.get_me())
-bot.send_message(text='Всем привет!', chat_id="@space_photos0")
-bot.send_document(chat_id="@space_photos0", document=open('images/epic_0.png', 'rb'))
+
+
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("--timeout", help="", type=int, default=14400)
+args = parser.parse_args()
+
+
+while True:
+    for root,dirs,files in os.walk('images'):
+        random.shuffle(files)
+        for file in files:
+            bot.send_document(chat_id="@space_photos0", document=open(f'images/{file}', 'rb'))
+            time.sleep(5)
+    time.sleep(args.timeout)
+
+
+
+
