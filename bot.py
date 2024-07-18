26b443487d12d403a18735c918893bd1fba159da
Vonard <129731100+Vonard@users.noreply.github.com>
2024-07-18 16:52:00 +0300

    Renamed CHAT_ID into TG_CHAT_ID

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ def main():
     load_dotenv()
     images_folder = os.environ['IMAGES_FOLDER']
     bot = telegram.Bot(token=os.environ['TELEGRAM_BOT_TOKEN'])
+    chat_id = os.environ['TG_CHAT_ID']
     parser = argparse.ArgumentParser(description="Launches the telegram bot, that sends pictures.")
     parser.add_argument("--timeout", help="Selects the time after which the bot starts sending pictures again.", type=int, default=14400)
     args = parser.parse_args()
@@ -18,7 +19,7 @@ def main():
             random.shuffle(files)
             for file in files:
                 with open(f'{images_folder}/{file}', 'rb') as file:
-                    bot.send_document(chat_id=os.environ['CHAT_ID'], document=file)
+                    bot.send_document(chat_id=chat_id, document=file)
                 time.sleep(5)
         time.sleep(args.timeout)
 
